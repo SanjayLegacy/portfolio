584406ef9055dea0b57bd86f9287f6c5a401b562
sanjay <sanjayashwin.s@gmail.com>
2025-04-23 20:04:08 +0530

    Mobile nav bar updates

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -21,13 +21,17 @@ export default function HeroSection() {
         <div className="flex flex-col text-black sm:w-[50%] h-full justify-center space-y-4 w-full pl-10 sm:pl-20">
           <div className="text-3xl">{"Hi, I'm"}</div>
           <TypeAnimation
-            sequence={["Sanjay Senthil Kumar", 1000]}
+            sequence={[
+              "Sanjay Senthil Kumar",
+              1000,
+              "Front End Developer",
+              1000,
+            ]}
             wrapper="span"
             speed={35}
-            className="font-medium text-transparent text-6xl bg-clip-text bg-gradient-to-r from-gray-800 to-gray-400"
+            className="font-medium text-transparent text-6xl bg-clip-text bg-gradient-to-r from-gray-800 to-gray-400 py-2"
             repeat={Infinity}
           />
-          <div className="text-2xl text-gray-600">Front End Developer</div>
           <div className="flex flex-row gap-x-4">
             <SocialIcon
               target="_blank"
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,9 +1,20 @@
 "use client";
 import Link from "next/link";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { Menu, X } from "lucide-react";
+import { useOnClickOutside } from "usehooks-ts";
 
 export default function NavBar() {
+  const ref = useRef<any>(null);
+
   const [scrolled, setScrolled] = useState<boolean>(false);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  useOnClickOutside(ref, toggleMenu);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -20,14 +31,52 @@ export default function NavBar() {
         scrolled ? "bg-gray-300 text-black" : "bg-black text-white"
       }`}
     >
-      <nav className="relative z-50 flex flex-row justify-center gap-x-8">
-        <Link href="#about-me" className="line-clamp-1">
-          About me
-        </Link>
+      <nav className="hidden md:flex relative z-50 flex-row justify-center gap-x-8">
+        <Link href="#about-me">About me</Link>
         <Link href="#experience">Experience</Link>
         <Link href="#skills">Skills</Link>
         <Link href="#projects">Projects</Link>
       </nav>
+
+      <div className="md:hidden flex items-center justify-end">
+        <button
+          onClick={toggleMenu}
+          className={`p-2 focus:outline-none border rounded-md ${
+            scrolled ? "border-black" : "border-white"
+          }`}
+        >
+          {isOpen ? <X size={24} /> : <Menu size={24} />}
+        </button>
+      </div>
+
+      <div className={`md:hidden ${isOpen ? "block" : "hidden"}`} ref={ref}>
+        <div className="py-2">
+          <Link
+            href="#about-me"
+            className="block py-2 px-4"
+            onClick={toggleMenu}
+          >
+            About me
+          </Link>
+          <Link
+            href="#experience"
+            className="block py-2 px-4"
+            onClick={toggleMenu}
+          >
+            Experience
+          </Link>
+          <Link href="#skills" className="block py-2 px-4" onClick={toggleMenu}>
+            Skills
+          </Link>
+          <Link
+            href="#projects"
+            className="block py-2 px-4"
+            onClick={toggleMenu}
+          >
+            Projects
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
